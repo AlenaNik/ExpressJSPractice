23be0548372f57cd31429bcc953a3ef8656a9b9b
AlenaNik <alena.nik.digital@gmail.com>
2019-05-13 15:57:36 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,45 @@
+const express = require('express');
+const app = express();
+const port = 300;
+
+app.use(express.json());
+
+const games = [
+        { id : 1, name : 'Arya'},
+        { id : 2, name : 'Sansa'},
+        { id : 3, name : 'Cercel'}
+];
+
+app.get('/api/game', (req, res) => {
+    res.send(games)
+});
+
+app.get('/api/game/:id', (req, res) => {
+    const game = games.find(target => target.id === parseInt(req.params.id));
+    res.send(games)
+});
+
+app.post('/api/game', (req, res) => {
+    const game = {
+        id : games.length +1,
+        name : req.body.name
+    };
+    games.push(game)
+    res.send(game)
+});
+
+app.put('/api/game/:id', (req, res) => {
+    const game = games.find(target => target.id === parseInt(req.params.id));
+    game.name = req.body.name;
+    res.send(game);
+});
+
+app.delete('/api/game/:id', (req, res) => {
+    const game = games.find(target => target.id === parseInt(req.params.id));
+
+   const index = games.indexOf(game)
+    games.splice(index, 1)
+    res.send(game);
+});
+
+app.listen(port, () => console.log(`The server is running on ${port}` ));
